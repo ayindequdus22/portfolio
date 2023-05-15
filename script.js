ada5c1458cdbe79a4288381154a3edebb8044bed
ayindequdus22 <abdulqudusayinde22@gmail>
2023-05-15 14:01:42 +0100

    final commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var typed = new Typed(".typing",{
+    strings:["","Developer.","Ethical Hacker.","YouTuber."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+var typed = new Typed(".typejb",{
+    strings:["Developer.","YouTuber.","Freelancer."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//         var typed = new Typed(".typechange",{
+//             strings:["Designer"],
+//             typeSpeed:100,
+//             BackSpeed:60,
+//             loop:true
+//         }); 
+//     } else {
+//     }
+//   }
+// const x = window.matchMedia("(max-width: 500px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes*/
