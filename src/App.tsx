ddeb5725785f57ed9e5b789c91928c422e4ebf08
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-12-02 14:57:35 -0800

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 
 import Project from './components/project/Project'
-import Footer from './components/footer/Footer'
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import Home from './Home'
 import About from './components/about/About'
+import Footer from './components/footer/Footer'
 
 function App() {
   const router = createBrowserRouter([{
